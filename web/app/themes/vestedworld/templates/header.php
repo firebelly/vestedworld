2f62870a6db0379cef3b2ff9899dc320c76ee68b
Nate Beaty <nate@firebellydesign.com>
2016-03-29 18:36:10 -0500

    add sign up link to nav outside main menu

--- web/app/themes/vestedworld/templates/header.php
+++ web/app/themes/vestedworld/templates/header.php
@@ -1,7 +1,7 @@
 <header class="site-header" role="banner">
   <div class="container">
-    <h1>
-      <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+    <h1 class="logo">
+      <a href="<?= esc_url(home_url('/')); ?>">
         <svg class="icon icon-logo" role="img"><use xlink:href="#icon-logo"></use></svg><svg class="icon icon-vestedworld" role="img"><title><?php bloginfo('name'); ?></title><use xlink:href="#icon-vestedworld"></use></svg>
       </a>
     </h1>
@@ -11,6 +11,10 @@
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
       endif;
       ?>
+      <div class="nav-actions">
+        <a class="sign-up" href="/sign-up/">Sign Up</a>
+        <a class="show-search" href="/search/"><span class="sr-only">Search</span><svg class="icon icon-search"><use xlink:href="#icon-search"></svg></a>
+      </div>
     </nav>
   </div>
 </header>
