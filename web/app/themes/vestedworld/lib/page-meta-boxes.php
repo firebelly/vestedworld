c0a002a857e7f8f76de9ae9e33841d4908fa783e
Nate Beaty <nate@firebellydesign.com>
2016-04-20 11:04:23 -0500

    stop hiding content area as it’s used by fb_metatags

--- web/app/themes/vestedworld/lib/page-meta-boxes.php
+++ web/app/themes/vestedworld/lib/page-meta-boxes.php
@@ -18,7 +18,7 @@ namespace Firebelly\PostTypes\Pages;
      'priority'      => 'high',
      'show_names'    => true, // Show field names on the left
      'fields'        => array(
-    
+
        // General page fields
        array(
          'name' => 'Target',
@@ -94,36 +94,33 @@ namespace Firebelly\PostTypes\Pages;
  * Hide editor on home page -- dont need it.
  * adapted from: https://gist.github.com/ramseyp/4060095
  */
-add_action( 'do_meta_boxes', __NAMESPACE__ . '\hide_editor' );
+// add_action( 'do_meta_boxes', __NAMESPACE__ . '\hide_editor' );
 function hide_editor() {
-  	//leave if we are not editing a page...
+  	// Leave if we are not editing a page...
   	if( get_current_screen()->id != 'page') return;
 
   	// Get the Post ID.
-  	if (isset($_GET['post'])) 
+  	if (isset($_GET['post']))
   		$post_id = $_GET['post'];
   	elseif (isset($_POST['post_ID']))
   		$post_id = $_POST['post_ID'];
   	else $post_id = false;
 
-  	//No post id? (e.g. editing a new, unsaved page) Get outta here!
+  	// No post id? (e.g. editing a new, unsaved page) Get outta here!
   	if(!$post_id) return;
-  
+
   	// Hide the editor on a page with a specific page template
   	// Get the name of the Page Template file.
-  	$template_file = get_post_meta($post_id, '_wp_page_template', true); 
+  	$template_file = get_post_meta($post_id, '_wp_page_template', true);
   	$post = get_post($post_id);
   	$slug = $post->post_name;
-  	$exclude_on = array( // any template filenames or slugs here will be have the editor excluded
+  	$exclude_on = array( // Any template filenames or slugs here will be have the editor excluded
   		'front-page.php',
   		'about-us',
   		'resources',
       'faqs',
   		);
-  	if(in_array($template_file,$exclude_on) || in_array($slug,$exclude_on)){ 
+  	if(in_array($template_file,$exclude_on) || in_array($slug,$exclude_on)){
     	remove_post_type_support('page', 'editor');
   	}
 }
-
-//Notice: Undefined index: post_ID in /Users/bryant/Sites/vestedworld/web/app/themes/vestedworld/lib/page-meta-boxes.php on line 59
-
