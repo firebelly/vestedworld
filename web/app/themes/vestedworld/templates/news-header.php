d634f10504a48080d3c0adb8baf5f699f192a757
Nate Beaty <nate@firebellydesign.com>
2016-04-25 17:39:05 -0500

    some initial styling of news filters + search, show search term in form, make News a link to news index, style alignleft and alignright images

--- web/app/themes/vestedworld/templates/news-header.php
+++ web/app/themes/vestedworld/templates/news-header.php
@@ -1,14 +1,14 @@
 <header class="news-filter">
   <form action="<?= get_permalink(get_option('page_for_posts')) ?>" method="get">
-    <h1>News</h1>
+    <h1><a href="<?= get_permalink(get_option('page_for_posts')) ?>">News</a></h1>
     <fieldset>
       <div class="filter">
         <label>Filter</label>
-        <?php wp_dropdown_categories(); ?>
+        <div class="select-wrapper"><?php wp_dropdown_categories(); ?></div>
       </div>
       <div class="search">
         <label class="sr-only">Search</label>
-        <input type="text" name="s" placeholder="Search">
+        <input type="text" name="s" placeholder="Search" value="<?= get_search_query() ?>">
       </div>
     </fieldset>
   </form>
