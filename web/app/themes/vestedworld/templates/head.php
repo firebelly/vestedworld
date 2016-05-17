0715420323e6da841455eae49b21fcbdd427a2d7
Matt Soria <matt.m.soria@gmail.com>
2016-05-17 17:36:24 -0500

    Styling gravity forms forms and functionality

--- web/app/themes/vestedworld/templates/head.php
+++ web/app/themes/vestedworld/templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php gravity_form_enqueue_scripts( 1, true ); ?>
   <?php wp_head(); ?>
 </head>
