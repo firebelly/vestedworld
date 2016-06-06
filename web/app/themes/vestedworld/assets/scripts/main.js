e1b04f1b3ad4812821b3228b86dac3488f96e92e
Nate Beaty <nate@firebellydesign.com>
2016-06-06 17:20:01 -0500

    animate with chartist

--- web/app/themes/vestedworld/assets/scripts/main.js
+++ web/app/themes/vestedworld/assets/scripts/main.js
@@ -151,11 +151,50 @@ var VestedWorld = (function($) {
       var numArr = $(this).closest('.chart-row').find('.stat-num').map(function() {
           return parseInt($(this).text().replace('%',''));
         }).get();
-      new Chartist.Pie(this, { series: numArr }, {
+      var chart = new Chartist.Pie(this, { series: numArr }, {
         donut: true,
         donutWidth: 8,
         showLabel: false,
         total: $(this).attr('data-total')
+      });
+
+      chart.on('draw', function(data) {
+        if(data.type === 'slice') {
+          // Get the total path length in order to use for dash array animation
+          var pathLength = data.element._node.getTotalLength();
+
+          // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+          data.element.attr({
+            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+          });
+
+          // Create animation definition while also assigning an ID to the animation for later sync usage
+          var animationDefinition = {
+            'stroke-dashoffset': {
+              id: 'anim' + data.index,
+              dur: 2000,
+              from: -pathLength + 'px',
+              to:  '0px',
+              easing: Chartist.Svg.Easing.easeOutQuint,
+              // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+              fill: 'freeze'
+            }
+          };
+
+          // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+          if(data.index !== 0) {
+            animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+          }
+
+          // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+          data.element.attr({
+            'stroke-dashoffset': -pathLength + 'px'
+          });
+
+          // We can't use guided mode as the animations need to rely on setting begin manually
+          // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+          data.element.animate(animationDefinition, false);
+        }
       });
     });
     $('.donut-chart-stacked').each(function() {
