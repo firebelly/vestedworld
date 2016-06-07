63e4d9e66a32d6b9da2fdcde88f873fed0f3c2cf
Nate Beaty <nate@firebellydesign.com>
2016-06-07 13:08:23 -0500

    more chartist animations

--- web/app/themes/vestedworld/assets/scripts/main.js
+++ web/app/themes/vestedworld/assets/scripts/main.js
@@ -160,15 +160,10 @@ var VestedWorld = (function($) {
 
       chart.on('draw', function(data) {
         if(data.type === 'slice') {
-          // Get the total path length in order to use for dash array animation
           var pathLength = data.element._node.getTotalLength();
-
-          // Set a dasharray that matches the path length as prerequisite to animate dashoffset
           data.element.attr({
             'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
           });
-
-          // Create animation definition while also assigning an ID to the animation for later sync usage
           var animationDefinition = {
             'stroke-dashoffset': {
               id: 'anim' + data.index,
@@ -180,30 +175,45 @@ var VestedWorld = (function($) {
               fill: 'freeze'
             }
           };
-
-          // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
-          if(data.index !== 0) {
-            animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-          }
-
-          // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
           data.element.attr({
             'stroke-dashoffset': -pathLength + 'px'
           });
-
-          // We can't use guided mode as the animations need to rely on setting begin manually
-          // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
           data.element.animate(animationDefinition, false);
         }
       });
     });
     $('.donut-chart-stacked').each(function() {
-      new Chartist.Pie(this, { series: [{ className: $(this).attr('data-class'), data: [$(this).attr('data-value')] }] }, {
+      var chart = new Chartist.Pie(this, { series: [{ className: $(this).attr('data-class'), data: [$(this).attr('data-value')] }] }, {
         donut: true,
         donutWidth: 8,
         showLabel: false,
         total: $(this).attr('data-total')
       });
+
+      chart.on('draw', function(data) {
+        if(data.type === 'slice') {
+          var pathLength = data.element._node.getTotalLength();
+          data.element.attr({
+            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+          });
+          var animationDefinition = {
+            'stroke-dashoffset': {
+              id: 'anim' + data.index,
+              dur: 2000,
+              from: -pathLength + 'px',
+              to:  '0px',
+              easing: Chartist.Svg.Easing.easeOutQuint,
+              // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+              fill: 'freeze'
+            }
+          };
+          data.element.attr({
+            'stroke-dashoffset': -pathLength + 'px'
+          });
+          data.element.animate(animationDefinition, false);
+        }
+      });
+
     });
 
   } // end init()
